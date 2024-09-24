668d40275dba2fbbb91252c4fd3ab856fdd56502
Firmiana <wutong20@mail.ustc.edu.cn>
2024-09-24 21:02:48 +0800

    change EDITOR to nvim

--- fish/config.fish
+++ fish/config.fish
@@ -2,11 +2,12 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-set -g fish_greeting ""
-
 if status --is-login
     if test -z "$DISPLAY" -a $XDG_VTNR=1
         exec Hyprland
     end
 end
 
+set -g fish_greeting ""
+
+set -U EDITOR nvim
