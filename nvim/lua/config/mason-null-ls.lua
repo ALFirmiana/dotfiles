04e614dd281c7cf25ab449fa33bee0cb702a4803
Firmiana <wutong20@mail.ustc.edu.cn>
2024-09-25 14:54:08 +0800

    nvim:add prettier

--- nvim/lua/config/mason-null-ls.lua
+++ nvim/lua/config/mason-null-ls.lua
@@ -28,7 +28,7 @@ mason.setup()
 mason_null_ls.setup({
 	-- A list of sources to install if they're not already installed.
 	-- This setting has no relation with the `automatic_installation` setting.
-	ensure_installed = { "clang-format", "stylua", "latexindent" },
+	ensure_installed = { "clang-format", "stylua", "latexindent", "prettier" },
 	-- Run `require("null-ls").setup`.
 	-- Will automatically install masons tools based on selected sources in `null-ls`.
 	-- Can also be an exclusion list.
