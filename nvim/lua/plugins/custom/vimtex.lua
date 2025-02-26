f0ac4ad1f27a8536c70affd37505d24c8e15ade4
Firmiana <wutong20@mail.ustc.edu.cn>
2025-02-26 12:56:24 +0800

    common update

--- nvim/lua/plugins/custom/vimtex.lua
+++ nvim/lua/plugins/custom/vimtex.lua
@@ -1,7 +1,7 @@
 return {
 	{
 		"lervag/vimtex",
-		ft = { "latex" },
+		ft = { "tex", "latex" },
 		init = function()
 			vim.g.vimtex_view_method = "zathura"
 			vim.g.vimtex_quickfix_open_on_warning = 0
