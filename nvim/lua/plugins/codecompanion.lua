d417e5b7467b5684394dcf2c117b2607bb0c1309
Firmiana <wutong20@mail.ustc.edu.cn>
2025-06-03 13:19:44 +0800

    nvim:add codecompanion.lua history extension

--- nvim/lua/plugins/codecompanion.lua
+++ nvim/lua/plugins/codecompanion.lua
@@ -4,6 +4,7 @@ return {
 		"nvim-lua/plenary.nvim",
 		"nvim-treesitter/nvim-treesitter",
 		"echasnovski/mini.diff",
+		"ravitemer/codecompanion-history.nvim",
 	},
 	config = function()
 		require("mini.diff").setup({
@@ -64,6 +65,43 @@ return {
 					layout = "vertical", -- vertical|horizontal split for default provider
 					opts = { "internal", "filler", "closeoff", "algorithm:patience", "followwrap", "linematch:120" },
 					provider = "mini_diff", -- default|mini_diff
+				},
+			},
+			extensions = {
+				history = {
+					enabled = true,
+					opts = {
+						-- Keymap to open history from chat buffer (default: gh)
+						keymap = "<leader>h",
+						-- Keymap to save the current chat manually (when auto_save is disabled)
+						save_chat_keymap = "sc",
+						-- Save all chats by default (disable to save only manually using 'sc')
+						auto_save = true,
+						-- Number of days after which chats are automatically deleted (0 to disable)
+						expiration_days = 0,
+						-- Picker interface (auto resolved to a valid picker)
+						picker = "telescope", --- ("telescope", "snacks", "fzf-lua", or "default")
+						---Automatically generate titles for new chats
+						auto_generate_title = true,
+						title_generation_opts = {
+							---Adapter for generating titles (defaults to current chat adapter)
+							adapter = nil, -- "copilot"
+							---Model for generating titles (defaults to current chat model)
+							model = nil, -- "gpt-4o"
+							---Number of user prompts after which to refresh the title (0 to disable)
+							refresh_every_n_prompts = 0, -- e.g., 3 to refresh after every 3rd user prompt
+							---Maximum number of times to refresh the title (default: 3)
+							max_refreshes = 3,
+						},
+						---On exiting and entering neovim, loads the last chat on opening chat
+						continue_last_chat = false,
+						---When chat is cleared with `gx` delete the chat from history
+						delete_on_clearing_chat = false,
+						---Directory path to save the chats
+						dir_to_save = vim.fn.stdpath("data") .. "/codecompanion-history",
+						---Enable detailed logging for history extension
+						enable_logging = false,
+					},
 				},
 			},
 		})
