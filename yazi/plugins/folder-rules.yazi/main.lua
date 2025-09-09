0610eb7ae1c7ba33d4a8e243c7824f1a7b1e734d
Firmiana <wutong20@mail.ustc.edu.cn>
2025-09-09 13:13:00 +0800

    yazi:more folder-rules

--- yazi/plugins/folder-rules.yazi/main.lua
+++ yazi/plugins/folder-rules.yazi/main.lua
@@ -3,6 +3,8 @@ local function setup()
 		local cwd = cx.active.current.cwd
 		if cwd:ends_with("Downloads") or cwd:ends_with("Bangumi") then
 			ya.emit("sort", { "mtime", reverse = true, dir_first = false })
+		elseif cwd:ends_with("GroupMeeting") then
+			ya.emit("sort", { "alphabetical", reverse = true, dir_first = true })
 		else
 			ya.emit("sort", { "alphabetical", reverse = false, dir_first = true })
 		end
