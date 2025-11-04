1eb1ec5b2f8e6580cafcffca0e9cb74a0b7a36fa
Firmiana <wutong20@mail.ustc.edu.cn>
2025-11-04 14:17:03 +0800

    nvim:nothing special

--- nvim/lua/plugins/custom/typst-preview.lua
+++ nvim/lua/plugins/custom/typst-preview.lua
@@ -4,7 +4,7 @@ return {
     ft = 'typst',
     config = function()
       require('typst-preview').setup {
-        open_cmd = 'zen --new-window %s',
+        open_cmd = 'zen-browser --new-window %s',
         dependencies_bin = { ['tinymist'] = 'tinymist' },
       }
       vim.keymap.set('n', '<leader>ll', '<CMD>TypstPreview<CR>', { desc = 'TypstPreview' })
