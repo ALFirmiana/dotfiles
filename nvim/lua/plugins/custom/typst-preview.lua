e64a51d36630bfaa94860538c637e7cfd27c9529
Firmiana <firmiana@Firmiana-MacBook-Air.local>
2026-09-11 11:29:34 +0800

    nvim:typst-preview in macos

--- nvim/lua/plugins/custom/typst-preview.lua
+++ nvim/lua/plugins/custom/typst-preview.lua
@@ -3,8 +3,15 @@ return {
     'chomosuke/typst-preview.nvim',
     ft = 'typst',
     config = function()
+      local open_cmd
+      if vim.fn.has 'mac' == 1 then
+        open_cmd = '/Applications/Zen.app/Contents/MacOS/zen --new-window %s'
+      else
+        open_cmd = 'zen-browser --new-window %s'
+      end
+
       require('typst-preview').setup {
-        open_cmd = 'zen-browser --new-window %s',
+        open_cmd = open_cmd,
         dependencies_bin = { ['tinymist'] = 'tinymist' },
       }
       vim.keymap.set('n', '<leader>ll', '<CMD>TypstPreview<CR>', { desc = 'TypstPreview' })
